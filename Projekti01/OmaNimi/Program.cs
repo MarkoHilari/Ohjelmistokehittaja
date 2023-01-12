aa4cdfbdf02f9e86e28e4cc2532af62fea46d708
Opiskelija <marko.hilari@edu.keuda.fi>
2023-01-12 11:31:38 +0200

    jotain

---
+++ Projekti01/OmaNimi/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Kysytään käyttäjäjän etunimeä
+        Console.Write("Anna Etunimesi: ");
+        string fname = Console.ReadLine();
+        //Kysytään käyttäjäjän sukunimeä
+        Console.Write("Anna Sukunimesi: ");
+        String sname = Console.ReadLine();
+        //Tulostetaan konsoliin nimi sekä tervehdys
+        Console.WriteLine("Heippa, " + fname + " " + sname);
+        Console.WriteLine("Heippa {0} {1}", fname, sname);
+        //Ohje käyttäjälle, että voi sulkea ikkunan
+        Console.WriteLine("Voit nyt sulkea ikkunan!");
+        Console.ReadLine();
+    }
+}
---
+++ Projekti01/YksiNumerot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("1");
+        Console.WriteLine("11");
+        Console.WriteLine("101");
+        Console.WriteLine("1001");
+        //Tästä alkaa kellonajan näyttö
+        /*ja
+         * Kestää alle saakka*/
+        var date1 = DateTime.Now;
+        Console.WriteLine(date1.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("fi-FI")));
+
+        Console.WriteLine(Math.Sqrt(50));
+
+
+    }
+}
